375a07fc60dd5ea14643e71540c4eaf7e740e708
tomporvaz <tomporvaz@gmail.com>
2020-04-29 21:02:56 -0400

    Fixed ExamFilter to load on initial load.  Enabled componentDidMount to load filter state.

--- src/ExamFilter.jsx
+++ src/ExamFilter.jsx
@@ -68,9 +68,9 @@ export default class ExamFilter extends React.Component{
     }
 
       //component did mount grabs fitlers from props, and builds state.filters
-/*       componentDidMount(){
+      componentDidMount(){
         this.getSemestersFilters(this.props.semester)
-      } */
+      }
 
       componentDidUpdate(prevProps){
         if(prevProps.semester !== this.props.semester){
